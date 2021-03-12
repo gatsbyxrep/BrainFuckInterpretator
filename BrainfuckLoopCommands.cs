521d467245ee691aa5d3965df39bf427cead1e14
russiancplusplus <gatsbynalda@gmail.com>
2021-03-12 12:20:50 +0300

    Loop commands have been added

--- BrainfuckLoopCommands.cs
+++ BrainfuckLoopCommands.cs
@@ -6,8 +6,35 @@ namespace func.brainfuck
 	{
 		public static void RegisterTo(IVirtualMachine vm)
 		{
-			vm.RegisterCommand('[', b => { });
-			vm.RegisterCommand(']', b => { });
+			var loopStack = new Stack<int>();
+			var loopTable = new Dictionary<int, int>();
+
+
+			for (int i = 0; i < vm.Instructions.Length; i++)
+			{
+				var instruction = vm.Instructions[i];
+
+				if (instruction.Equals('['))
+					loopStack.Push(i);
+				else if (instruction.Equals(']'))
+				{
+					var openBracket = loopStack.Pop();
+					loopTable[openBracket] = i;
+					loopTable[i] = openBracket;
+				}
+			}
+
+			vm.RegisterCommand('[', b =>
+			{
+				if (b.Memory[b.MemoryPointer] == 0)
+					b.InstructionPointer = loopTable[b.InstructionPointer];
+			});
+
+			vm.RegisterCommand(']', b => 
+			{
+				if (b.Memory[b.MemoryPointer] != 0)
+					b.InstructionPointer = loopTable[b.InstructionPointer];
+			});
 		}
 	}
 }
